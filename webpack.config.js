c7dab13f6be9a3cd4a155fba85a6a6b70d347fed
adrian92612 <adrian.villamin0612@gmail.com>
2023-11-14 22:07:54 +0800

    added asset loader and 2 assets

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 use:['style-loader','css-loader']
+            },
+            {
+                test: /\.(svg|png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
             }
         ]
     },
