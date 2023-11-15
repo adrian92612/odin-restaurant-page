ebedbc4fc0826c6c26f3d2fb474d41cfc7318be8
adrian92612 <adrian.villamin0612@gmail.com>
2023-11-15 14:54:51 +0800

    added a basic footer

---
+++ src/footer.js
@@ -0,0 +1,5 @@
+export default function createFooter() {
+    const footer = document.createElement('footer')
+    footer.innerText = 'Created by Adrian92612'
+    return footer
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './styles/main.css'
 import createHeader from './header'
+import createFooter from './footer'
 
 
 const root = document.getElementById('root')
 root.appendChild(createHeader())
+// root.appendChild()
+root.appendChild(createFooter())
 console.log('asdfdas')
