faab93798360800844840e74f928f0cbff7cd84e
adrian92612 <adrian.villamin0612@gmail.com>
2023-11-17 18:50:24 +0800

    added pop up nav

--- src/header.js
+++ src/header.js
@@ -1,29 +1,57 @@
-import icon from './assets/menu-icon.svg';
+import icon from "./assets/menu-icon.svg";
 
 const createLogo = () => {
-    const logo = document.createElement('div')
-    logo.classList.add('logo')
+  const logo = document.createElement("div");
+  logo.classList.add("logo");
 
-    const logoText = document.createElement('p')
-    logoText.innerText = `Amber's Steakhouse`
-    logo.appendChild(logoText)
-    return logo
-}
+  const logoText = document.createElement("p");
+  logoText.innerText = `Amber's Steakhouse`;
+  logo.appendChild(logoText);
+  return logo;
+};
+
+const createfloatingNav = () => {
+  const floatNav = document.createElement("div");
+  floatNav.classList.add("float-nav");
+
+  const homeBtn = document.createElement("button");
+  homeBtn.classList.add("home-btn", "nav-btn");
+  homeBtn.innerText = "Home";
+  floatNav.appendChild(homeBtn);
+
+  const menuBtn = document.createElement("button");
+  menuBtn.classList.add("menu-btn", "nav-btn");
+  menuBtn.innerText = "Menu";
+  floatNav.appendChild(menuBtn);
+
+  const aboutUsBtn = document.createElement("button");
+  aboutUsBtn.classList.add("about-us-btn", "nav-btn");
+  aboutUsBtn.innerText = "About Us";
+  floatNav.appendChild(aboutUsBtn);
+
+  const contactUsBtn = document.createElement("button");
+  contactUsBtn.classList.add("contact-us-btn", "nav-btn");
+  contactUsBtn.innerText = "Contact Us";
+  floatNav.appendChild(contactUsBtn);
+
+  return floatNav;
+};
 
 const createNav = () => {
-    const nav = document.createElement('nav')
-    
-    const menuBtn = document.createElement('img')
-    menuBtn.classList.add('menu-btn')
-    menuBtn.src = icon
+  const nav = document.createElement("nav");
 
-    return menuBtn
-}
+  const menuIcon = document.createElement("img");
+  menuIcon.classList.add("menu-icon");
+  menuIcon.src = icon;
+
+  return menuIcon;
+};
 
 export default function createHeader() {
-    const header = document.createElement('header')
+  const header = document.createElement("header");
 
-    header.appendChild(createLogo())
-    header.appendChild(createNav())
-    return header
+  header.appendChild(createLogo());
+  header.appendChild(createNav());
+  header.appendChild(createfloatingNav());
+  return header;
 }
